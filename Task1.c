348120d00e4825815945ba6d37af04100e6e16de
Syed Kamran Ahmed <k253016@nu.edu.pk>
2025-10-26 23:21:01 +0500

    Add files via upload

---
+++ Task1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    int j=0;
+    
+    for(i; i<11; i++){
+        for(j=0; j<i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
